17494888241cd3697cffba3a95aa1b9382b125c9
sachithra madusanka <sachithramadusanka345@gmail.com>
2025-07-27 08:31:16 +0100

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *🔥 CHALAH MD SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> *🔥 BLOOD XMD SPEED🚀 : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
